37fbb9ea7f60af372e6cefa04deb9f739be3ec66
Gabriel Mello <gabrielpontemello1@gmail.com>
2026-06-26 20:12:00 -0300

    feat(products): add product service

---
+++ internal/products/service.go
@@ -0,0 +1,19 @@
+package products
+
+import "context"
+
+type Service struct {
+	repo *Repo
+}
+
+func NewService(repo *Repo) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) List(ctx context.Context) ([]Product, error) {
+	return s.repo.List(ctx)
+}
+
+func (s *Service) Get(ctx context.Context, id string) (Product, error) {
+	return s.repo.Get(ctx, id)
+}
